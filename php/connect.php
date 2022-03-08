f21e10e26cdde15124ec545ac6abb32d375cbf64
Lesley <leskimuti@gmail.com>
2022-03-09 02:08:34 +0300

    Added heroku database inside phpmyadmin

--- php/connect.php
+++ php/connect.php
@@ -3,8 +3,21 @@
 //connection between php and mysql database
 $conn = new mysqli('localhost', 'root', '', 'farm_products');
 
-
 // If not connected show error
 if (!$conn) {
     die(mysqli_error($conn));
 }
+
+
+
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = true;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
