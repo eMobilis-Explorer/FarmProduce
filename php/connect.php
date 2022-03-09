b60e81b1aab52a0ebe57791858c1056d141fbdf6
Lesley <leskimuti@gmail.com>
2022-03-09 15:54:52 +0300

    Redoing the soon part

--- php/connect.php
+++ php/connect.php
@@ -1,22 +1,22 @@
 <?php
 
-// connection between php and mysql database
-$conn = new mysqli('localhost', 'root', '', 'farm_products');
+// // connection between php and mysql database
+// $conn = new mysqli('localhost', 'root', '', 'farm_products');
 
-// If not connected show error
-if (!$conn) {
-    die(mysqli_error($conn));
-}
+// // If not connected show error
+// if (!$conn) {
+//     die(mysqli_error($conn));
+// }
 
 
 
-//Get Heroku ClearDB connection information
-// $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-// $cleardb_server = $cleardb_url["host"];
-// $cleardb_username = $cleardb_url["user"];
-// $cleardb_password = $cleardb_url["pass"];
-// $cleardb_db = substr($cleardb_url["path"], 1);
-// // $active_group = 'default';
-// // $query_builder = true;
-// // Connect to DB
-// $conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+// Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+// $active_group = 'default';
+// $query_builder = true;
+// Connect to DB
+$conn = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
